8794266bb8ed0017a355e6ac149212c667718906
Michael Shor <mshor24@cgps.org>
2022-11-28 09:03:00 -0500

    first commit - checkpoint

--- unit2App/ViewController.swift
+++ unit2App/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        // mIcrtui
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
