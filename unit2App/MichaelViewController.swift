f995b3b1876137af4e1dda8cd047c3c1e13aa703
Michael Shor <mshor24@cgps.org>
2022-12-16 15:26:35 -0500

    final commit

---
+++ unit2App/MichaelViewController.swift
@@ -0,0 +1,111 @@
+//
+//  MichaelViewController.swift
+//  unit2App
+//
+//  Created by Michael on 12/2/22.
+//
+
+import UIKit
+
+
+// This creates the struct that will store the base information for the specifics of a person's account on this social media app
+struct person {
+    var name: String
+    var birthday: String
+    var age: Int
+    var location: String
+    var following: Int
+    var followers: Int
+    var numOfLikes: Int
+    var pic: UIImage
+    var hasPet: Bool
+}
+
+
+
+class MichaelViewController: UIViewController {
+    
+    // This is the instance of the struct that is defined above. This means that this is a specific case of the struct in which all of the variables defined in the struct are given specific values which are unique to the username "Bobby".
+    var Person1 = person(name: "Bobby", birthday: "Jan 7 2005", age: 25, location: "New York", following: 1003, followers: 5000, numOfLikes: 5000000, pic: (UIImage(named: "chad") ?? UIImage(systemName: "square.and.arrow.up.circle"))!, hasPet: true)
+    
+    
+    // The outlets for all the UILabels, UIImageViews, and UIButtons in this page
+    @IBOutlet weak var PersonNameLabel: UILabel!
+    
+ 
+    @IBOutlet weak var ProfileImage: UIImageView!
+    
+    @IBOutlet weak var Followers: UILabel!
+    @IBOutlet weak var Following: UILabel!
+    @IBOutlet weak var NumberOfLikes: UILabel!
+    
+    
+    @IBOutlet weak var VerifiedCheck: UIImageView!
+    
+    @IBOutlet weak var PetInfo: UIButton!
+    
+    @IBOutlet weak var BirthdayAgeGeneral: UIButton!
+    @IBOutlet weak var CurrentEmployment: UIButton!
+    @IBOutlet weak var LikesAndInterests: UIButton!
+    
+    //This function takes the values defined in the instance of the struct (the information on the user) and adds them to the outlets we created for the imageViews and for the labels which add the information onto the screen
+    func loadDataOntoView() {
+        ProfileImage.image = Person1.pic
+        PersonNameLabel.text = Person1.name
+        Followers.text = "\(Person1.followers) followers"
+        Following.text = "\(Person1.following) following"
+        NumberOfLikes.text = "\(Person1.numOfLikes) likes"
+    }
+    
+    // This function creates the border settings and border color of the profile image of the person's account
+    func setUpBorders() {
+        ProfileImage.layer.borderWidth = 5.0
+        ProfileImage.layer.cornerRadius = 10.0
+        ProfileImage.layer.borderColor = CGColor(red: 101.0/255.0, green: 172.0/255.0, blue: 230.0/255.0, alpha: 1.0)
+        PetInfo.layer.borderWidth = 5.0
+        PetInfo.layer.cornerRadius = 10.0
+        PetInfo.layer.borderColor = CGColor(red: 101.0/255.0, green: 172.0/255.0, blue: 230.0/255.0, alpha: 1.0)
+        BirthdayAgeGeneral.layer.borderWidth = 5.0
+        BirthdayAgeGeneral.layer.cornerRadius = 10.0
+        BirthdayAgeGeneral.layer.borderColor = CGColor(red: 101.0/255.0, green: 172.0/255.0, blue: 230.0/255.0, alpha: 1.0)
+        CurrentEmployment.layer.borderWidth = 5.0
+        CurrentEmployment.layer.cornerRadius = 10.0
+        CurrentEmployment.layer.borderColor = CGColor(red: 101.0/255.0, green: 172.0/255.0, blue: 230.0/255.0, alpha: 1.0)
+        LikesAndInterests.layer.borderWidth = 5.0
+        LikesAndInterests.layer.cornerRadius = 10.0
+        LikesAndInterests.layer.borderColor = CGColor(red: 101.0/255.0, green: 172.0/255.0, blue: 230.0/255.0, alpha: 1.0)
+    }
+
+    // In the viewDidLoad we are adding all of our functions which gave the buttons, labels, and images values from the instance. By calling these functions in the viewDidLoad we are adding them to our view controller as soon as the app is loaded
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpBorders()
+        loadDataOntoView()
+        addAllNames()
+        VerifiedCheckMark()
+        // Do any additional setup after loading the view.
+    }
+    
+    // In this function we are changing the text of each of the UIButtons based on the values of the instances in the struct
+    func addAllNames() {
+        BirthdayAgeGeneral.setTitle("\(Person1.name)'s birthday, age, and general info", for: .normal)
+        CurrentEmployment.setTitle("\(Person1.name)'s current employment", for: .normal)
+        LikesAndInterests.setTitle("\(Person1.name)'s likes and interests", for: .normal)
+    }
+    
+    // In this part of the code we create the logic for adding an image of a verified check next to the profile based on certain conditions. In this case, if the number of followers and the number of likes which are two variables defined in the instance of the struct are greater than or equal to 5000 and 1000000 respectively. Based on if these two conditions are met then the function either returns true or false.
+    func isVerified() -> Bool {
+        if(Person1.followers >= 5000 && Person1.numOfLikes >= 1000000){
+            return true
+        } else {
+            return false
+        }
+    }
+    //Based on the first part of the function we either have a true value returned or a false value, we then make a conditional which states that if the function above is true meaning that the followers and num of likes is greater than a certain value then we set a UIImage to the verifiedcheck mark
+    func VerifiedCheckMark() {
+        if (isVerified()) {
+            VerifiedCheck.image = UIImage(named: "VerifiedCheck") ?? UIImage(systemName: "square.and.arrow.up.circle")!
+            
+        }
+    }
+}
--- unit2App/ViewController.swift
+++ unit2App/ViewController.swift
@@ -1,19 +1,99 @@
 //
 //  ViewController.swift
-//  unit2App
+//  socialMedia
 //
-//  Created by Michael on 11/28/22.
+//  Created by Sam Schrager on 11/30/22.
 //
 
 import UIKit
 
+struct pet {
+    //this is where the variables that go into our struct
+    var name: String
+    var age: Int
+    var location: String
+    var followers: Int
+    var following: Int
+    var totalLikes: Int
+    var breed: String
+    var weight: Double
+    var pic: UIImage
+    var hasOwner: Bool
+}
+
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var ownerInfo: UIButton!
+    //these are all of the outlets that are on the screen
+    @IBOutlet weak var importantDates: UIButton!
+    @IBOutlet weak var petPlaces: UIButton!
+    @IBOutlet weak var dogTitle: UILabel!
+    @IBOutlet weak var petLikes: UIButton!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var followers: UILabel!
+    @IBOutlet weak var Likes: UILabel!
+    @IBOutlet weak var followingText: UILabel!
+    @IBOutlet weak var blueCheck: UIImageView!
+    @IBOutlet weak var add: UIImageView!
+    //instantiating pet1 to have the name klappa, an age of four, her location being central park, 2000000 followers, following three people, has a total number of likes of 40000000, have breed of bernalabradoodle, have a weight of 60 and an image called dog that if failed to load will display a filler image.
+    var pet1 = pet(name: "klappa", age: 4, location: "Central Park", followers: 2000000, following: 3, totalLikes: 40000000, breed: "bernalabadoodle", weight: 60, pic: (UIImage(named: "dog") ?? UIImage(systemName: "square.and.arrow.up.circle"))!, hasOwner: true)
+    func setUpBorders() {
+        //sets up the image borders and sets them to blue.
+        profileImage.layer.borderWidth = 5.0
+        profileImage.layer.cornerRadius = 10.0
+        profileImage.layer.borderColor = CGColor(red: 101.0/255.0, green: 172.0/255.0, blue: 230.0/255.0, alpha: 1.0)
+        //the next group is for each different button found on the bottom of the screen, it sets a blue border colour and a corner radius of 10. it is done four times for each button.
+        ownerInfo.layer.borderWidth = 5.0
+        ownerInfo.layer.cornerRadius = 10.0
+        ownerInfo.layer.borderColor = CGColor(red: 101.0/255.0, green: 172.0/255.0, blue: 230.0/255.0, alpha: 1.0)
+        petPlaces.layer.borderWidth = 5.0
+        petPlaces.layer.cornerRadius = 10.0
+        petPlaces.layer.borderColor = CGColor(red: 101.0/255.0, green: 172.0/255.0, blue: 230.0/255.0, alpha: 1.0)
+        petLikes.layer.borderWidth = 5.0
+        petLikes.layer.cornerRadius = 10.0
+        petLikes.layer.borderColor = CGColor(red: 101.0/255.0, green: 172.0/255.0, blue: 230.0/255.0, alpha: 1.0)
+        importantDates.layer.borderWidth = 5.0
+        importantDates.layer.cornerRadius = 10.0
+        importantDates.layer.borderColor = CGColor(red: 101.0/255.0, green: 172.0/255.0, blue: 230.0/255.0, alpha: 1.0)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        //calls all code functions
+        setUpBorders()
+        addAllNames()
+        profileImage.image = pet1.pic
+        verify()
+        //create the image for the add that is called duolingo
+        add.image = UIImage(named: "duolingo")
         // Do any additional setup after loading the view.
     }
-
+    func addAllNames() {
+        //adds to each button a new title that includes the pet name.
+        importantDates.setTitle("Important Dates for \(pet1.name)", for: .normal)
+        petPlaces.setTitle("Where \(pet1.name) likes to hang out", for: .normal)
+        petLikes.setTitle("What does \(pet1.name) like?", for: .normal)
+        //ads the pet name to the label
+        dogTitle.text = pet1.name
+        //adds the follower count, num of likes, and following to the coresponding labels
+        followers.text = "\(pet1.followers) followers"
+        Likes.text = "\(pet1.totalLikes) likes"
+        followingText.text = "\(pet1.following) following"
+        //this is where we add the pet name to all of the text boxes
+    }
+    func isVerified()-> Bool{
+        //checks to see if the account has enough likes and followers to be verified, returns yes if true, returns false if no.
+        if(pet1.followers >= 10000 && pet1.totalLikes >= 100000){
+            return true
+        }
+        return false
+    }
+    func verify(){
+        //checks if the person is verified and if yes displays the verified blue check.
+        if(isVerified()){
+            blueCheck.image = UIImage(named: "check")
+        }
+    }
 
 }
 
